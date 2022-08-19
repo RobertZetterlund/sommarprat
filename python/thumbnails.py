2fcae0ae1f10c26884e653bb6099433b767953f1
Robert Zetterlund <robert.zetterlund@outlook.com>
2022-08-19 13:46:33 +0200

    3x3 is better thumbnail size

--- python/thumbnails.py
+++ python/thumbnails.py
@@ -15,18 +15,18 @@ for year in years:
     f.close()
 
     files = []
-    # Take 16 images for 4x4 grid
-    for i in data[:16]:
+    # Take 9 images for 3x3 grid
+    for i in data[-9:]:
         filename = i.get('date') + ".jpg"
         files.append(str(year) + "/" + filename)
 
     new_im = Image.new('RGB', (512, 512))
 
     index = 0
-    for i in range(0, 512, 128):
-        for j in range(0, 512, 128):
+    for i in range(0, 513, 171):
+        for j in range(0, 513, 171):
             im = Image.open(files[index])
-            im.thumbnail((128, 128))
+            im.thumbnail((171, 171))
             new_im.paste(im, (i, j))
             index += 1
 
